81267be48f23c2c5541c692f5956f3972e517377
Bilha <bilha.leposo@student.moringaschool.com>
2024-03-27 06:26:17 +0300

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+ var customername = 'bob';
+ 
+
+ function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase ();
+ }
+
+ function setBestCustomer () {
+    bestCustomer = 'not bob';
+ }
+
+ function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+ }
+
+
+ const leastFavoriteCustomer = 'someone';
+
+ function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'someone else';
+ }
+
+ module.exports = {
+    customerName,
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    leastFavoriteCustomer,
+    changeLeastFavoriteCustomer,
+ }
